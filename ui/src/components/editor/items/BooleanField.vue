0e23b1b04af84817b2f1e16d20efb415d3e3d83f
Hansin1997 <845612500@qq.com>
2022-02-23 01:47:18 +0800

    布尔值编辑

---
+++ ui/src/components/editor/items/BooleanField.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+import {props} from "./common";
+export default {
+  name: "BooleanField",
+  props:{
+    ...props
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
